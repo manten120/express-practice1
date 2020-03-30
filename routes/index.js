5de1abdfc5f414ad254a385b36fafaa18ffff106
stacru <hamaw8888@gmail.com>
2020-03-31 07:56:13 +0900

    商品検索機能を実装

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,28 @@ router.get('/', function(req, res, next) {
   data = {
     title: '〇〇商店',
     itemList: itemList,
+    searchedItemName: '',
+    matchedItemList: ''
+  }
+  res.render('index', data);
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  const searchedItemName = req.body['search'];
+
+  const matchedItemList = itemList.filter((itemObj) => {
+    return itemObj.name.includes(searchedItemName);
+  })
+
+
+  console.log(matchedItemList);
+
+  data = {
+    title: '〇〇商店',
+    itemList: itemList,
+    searchedItemName: searchedItemName,
+    matchedItemList: matchedItemList
   }
   res.render('index', data);
 });
