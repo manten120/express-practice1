3c1310ea0f7dde9df1c6fca142d089aa353127a8
stacru <hamaw8888@gmail.com>
2020-04-01 22:07:50 +0900

    Ajaxを使った検索ボックスを追加

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var itemRouter = require('./routes/item');
+var ajaxRouter = require('./routes/ajax');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/item', itemRouter);
+app.use('/ajax', ajaxRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- itemlist.js
+++ itemlist.js
@@ -1,11 +1,11 @@
 const itemList = [
-  { name: '消しゴム', price: 100 },
-  { name: '鉛筆', price: 120 },
-  { name: 'ノート', price: 200 },
-  { name: '筆箱', price: 1500 },
-  { name: '下敷き', price: 110 },
-  { name: '定規', price: 100 },
-  { name: '筆箱2', price: 1500 },
+  { id: 0, name: '消しゴム', price: 100 },
+  { id: 1, name: '鉛筆', price: 120 },
+  { id: 2, name: 'ノート', price: 200 },
+  { id: 3, name: '筆箱', price: 1500 },
+  { id: 4, name: '下敷き', price: 110 },
+  { id: 5, name: '定規', price: 100 },
+  { id: 6, name: '筆箱2', price: 1500 }
 ];
 
 module.exports = { itemList }
